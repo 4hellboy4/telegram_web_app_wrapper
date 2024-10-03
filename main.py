cef587de57395029d6d7784d2b45df3ab6f9ff60
4hellboy4 <gsv132005@gmail.com>
2024-10-03 12:16:20 +0300

    fix: added deeplink

--- main.py
+++ main.py
@@ -1,42 +1,44 @@
 import asyncio
+from email import message_from_string
 
 from aiogram import types
-from aiogram.utils.deep_linking import create_start_link
-from aiogram.utils.payload import decode_payload
-
+from aiogram.utils.deep_linking import create_start_link, decode_payload
 from tg_bot import config
 from tg_bot.loader import dp, bot
+from aiogram.filters import CommandStart
 from aiogram.filters import Command
 from tg_bot.admin.kb import admin_kb
 from tg_bot.user.kb import user_kb
 
-# запкускаем кнопку web_app сбоку от поля ввода
+# Запускаем кнопку web_app сбоку от поля ввода
 async def set_web_app_button():
     await bot.set_chat_menu_button(
         menu_button=types.MenuButtonWebApp(
             text="Играть",  # Текст на кнопке
-            web_app=types.WebAppInfo(url='ССЫЛКА') # вставляем ссылку на веб-приложение
+            web_app=types.WebAppInfo(url='ССЫЛКА')  # Вставляем ссылку на веб-приложение
         )
     )
-
 
 # Генерация deeplink с параметром startapp
 async def generate_deeplink(user_id):
     # Генерация ссылки с переданным user_id в качестве параметра
-    link = await create_start_link(bot, f'startapp={user_id}', encode=True)
+    link = await create_start_link(bot, f'start={user_id}', encode=True)  # Используем start=
     return link
 
-
-# Команда /start с проверкой ID пользователя является он админом или юзером
+# Обработка команды /start с deep linking
 @dp.message(Command(commands=['start']))
 async def start_command(message: types.Message):
-    await set_web_app_button() # запуск кнопки
+    await set_web_app_button()  # запуск кнопки
 
-    # Извлекаем параметр startapp, если он передан
-    start_param = message.text.split('startapp=')[-1] if 'startapp=' in message.text else None
+    url_code = await generate_deeplink(message.from_user.id)
+
+    # Извлекаем аргументы после команды /start
+    start_param = message.text.split(' ')[1] if len(message.text.split(' ')) > 1 else None
+
+    await message.answer(f'Привет, ваша генеральная ссылка - {url_code}')
 
     user_id = message.from_user.id
-    # проверка на админа
+    # Проверка на админа
     if user_id in config.admins:
         await message.answer("Привет, ты находишься на странице админа", reply_markup=admin_kb)
     else:
--- tg_bot/admin/kb.py
+++ tg_bot/admin/kb.py
@@ -1,5 +1,6 @@
 from aiogram.utils.keyboard import InlineKeyboardBuilder
+from aiogram import types
 
 admin_kb_builder = InlineKeyboardBuilder()
-admin_kb_builder.button(text="Протестировать", callback_data="Протестировать", web_app="ССЫЛКА")
+admin_kb_builder.button(text="Протестировать", callback_data="Протестировать", web_app=types.WebAppInfo(url='ССЫЛКА'))
 admin_kb = admin_kb_builder.as_markup(resize_keyboard=True)
